7de1d22f3adf6010a1074b0d69affe34f9743c0a
suchithra12 <sthammaiah@qwinix.io>
2016-05-20 15:19:54 +0530

    app.js changes

--- server/app.js
+++ server/app.js
@@ -10,12 +10,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var path = require('path');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
+
+path.join(__dirname, 'app/public/index.html')
 
 // Setup server
 var app = express();
